c86e254ee3082690ef07e2ddc47dfe2d514fcf0a
Shift Poster <86fullers_secrets@icloud.com>
2026-09-01 23:14:39 -0400

    test: added test

---
+++ tests/test_level.py
@@ -0,0 +1,50 @@
+from argparse import ArgumentParser
+from itertools import chain
+
+import pytest
+
+from log_level import Level
+
+try:
+    import loguru  # type: ignore # noqa: F401
+
+    LOGURU_INSTALLED: bool = True
+except ImportError:
+    LOGURU_INSTALLED: bool = False
+
+
+@pytest.mark.skipif(
+    not LOGURU_INSTALLED, reason="'loguru' needs to be installed for this test."
+)
+def test_loguru():
+    assert Level.TRACE
+
+
+@pytest.mark.skipif(
+    LOGURU_INSTALLED, reason="'loguru' can't be installed for this test."
+)
+def test_no_loguru():
+    assert not hasattr(Level, "TRACE")
+
+
+def test_pydantic():
+    pydantic = pytest.importorskip("pydantic")
+
+    class Cls(pydantic.BaseModel):
+        level: Level = Level.field()
+
+    Cls(level=Level.WARN)
+
+
+@pytest.mark.parametrize(
+    "level",
+    chain(Level, Level.__members__.keys(), map(str.lower, Level.__members__.keys())),
+)
+def test_argparse(level: str | int | Level):
+    parser = ArgumentParser()
+    Level.add_argument(parser)
+    args = parser.parse_args(("--log-level", str(level)))
+    assert Level(args.log_level) == Level(level)
+
+
+def test_basesettings(): ...
